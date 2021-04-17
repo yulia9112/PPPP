75011bedbaaac0e7bad1bac0d782d1d19272c864
Yulia <60868878+yulia9112@users.noreply.github.com>
2021-04-17 19:41:11 +0300

    Add files via upload

---
+++ Laba2/Header.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+void Metod(double a, double b, double c, double d, double e, double f, string* ans);
